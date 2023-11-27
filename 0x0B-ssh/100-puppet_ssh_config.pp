b8b43610444383a9b29864eccc6ea1abd6fb59fb
GibborKratos <gibbsonanita@gmail.com>
2023-11-27 02:19:19 +0000

    using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# using puppet to make canges to our config file
+
+file { 'etc/ssh/ssh_config':
+ensure => present,
+content =>"
+       #SSH client configuration
+host*
+IdentityFile ~/.ssh/school
+PasswordAuthentication no
+}
