4c792a665ced3680f3bc9c5c89381ce5ccb74507
GibborKratos <gibbsonanita@gmail.com>
2023-11-27 21:54:54 +0000

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,11 +1,6 @@
-exec { 'kill_process':
-  command     => 'pkill -f killmenow',
-  refreshonly => true,
-  subscribe   => Exec['start_my_process'],
+# Execute a command
+exec { 'pkill killmenow':
+	path => '/usr/bin:/usr/sbin:/bin'
 }
 
-exec { 'start_my_process':
-  command => 'your_command_to_start_killmenow_process',
-  onlyif  => 'pgrep -f killmenow',
-}
 
