9fd075d3e922e776814101a5295c08b5d3ae242a
GibborKratos <gibbsonanita@gmail.com>
2023-11-27 21:59:09 +0000

    update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,10 @@
-# Execute a command
-exec { 'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+class killmenow {
+  exec { 'killmenow_process':
+    command     => '/usr/bin/pkill killmenow',
+    refreshonly => true,
+    onlyif      => '/usr/bin/pgrep killmenow',
+  }
 }
 
+include killmenow
 
