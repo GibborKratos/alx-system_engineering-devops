d92e27eb889213aedaa38bc9dcd7815b724facf0
GibborKratos <gibbsonanita@gmail.com>
2024-03-05 13:28:27 +0000

    0x16 api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+# "solving task 0"
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                            headers={"User-Agent": "Mozilla/5.0"},
-                            allow_redirects=False)
-    
-    if sub_info.status_code == 200: 
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
