73debba865a6ea9003b3355fe801de94b3663147
GibborKratos <gibbsonanita@gmail.com>
2024-03-05 23:24:37 +0000

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    Sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit)
-                            headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    (
-    allow_redirects=False)
-    if sub_info.status_code == 404:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    return sub_info.json().get("data")
+    results = response.json().get("data")
     return results.get("subscribers")
