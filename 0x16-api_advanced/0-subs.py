bcb2e096f5817d182a31f92e49974500d51ba12a
GibborKratos <gibbsonanita@gmail.com>
2024-03-05 13:24:58 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,13 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+                            headers={"User-Agent": "Mozilla/5.0"},
                             allow_redirects=False)
     
-    if sub_info.status_code != 200:  # If status code is not 200 (OK), return 0
+    if sub_info.status_code == 200: 
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
-
-
