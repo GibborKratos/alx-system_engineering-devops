242ff4bc6b355dfc85a8a2dbab6db62ac7ddabf4
GibborKratos <gibbsonanita@gmail.com>
2024-03-07 21:49:43 +0000

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,6 @@ def number_of_subscribers(subreddit):
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-	data = response.json()
-	subscribers = data['data']['subscribers']
-        return subscribers
-    else:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
